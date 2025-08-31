6ba03f5dcf05b89e7564d254320b57796f03fdf8
KakoJC <g553901908@gmail.com>
2025-08-31 17:29:59 +0800

    修复面光源尺寸与实际尺寸不对应的bug

--- Assets/Scripts/AreaLight/AreaLight.cs
+++ Assets/Scripts/AreaLight/AreaLight.cs
@@ -3,18 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(MeshRenderer)), ExecuteAlways]
+[RequireComponent(typeof(MeshRenderer)),RequireComponent(typeof(MeshRenderer)), ExecuteAlways]
 public class AreaLight : MonoBehaviour
 {
     public AreaLightType areaLightType = AreaLightType.RECT;
     [Min(0.0f)]
     public float intensity = 1.0f;
     [Min(0.0f)]
-    public float range;
+    public float range = 10;
     [Min(0.0f)]
-    public float rangeAttenuationScale;
+    public float rangeAttenuationScale = 1;
     [Min(0.0f)]
-    public float rangeAttenuationBias;
+    public float rangeAttenuationBias = 1;
     public Color color = Color.white;
 
     private Transform m_Transform;
@@ -30,6 +30,7 @@ public class AreaLight : MonoBehaviour
     }
 
     private MeshRenderer m_MeshRenderer;
+    private Mesh m_Mesh;
     
     public void GetPosAndSize(out Vector3 pos, out Vector2 size)
     {
@@ -40,11 +41,21 @@ public class AreaLight : MonoBehaviour
         pos = m_Transform.position;
         size = Vector2.zero;
 
-        if (m_MeshRenderer == null)
+        //用这个是包围盒的尺寸，如果mesh旋转了，尺寸会不对
+        // if (m_MeshRenderer == null)
+        // {
+        //     m_MeshRenderer = GetComponent<MeshRenderer>();
+        // }
+        //size = m_MeshRenderer.bounds.size;
+
+        if (m_Mesh == null)
         {
-            m_MeshRenderer = GetComponent<MeshRenderer>();
+            m_Mesh = GetComponent<MeshFilter>().sharedMesh;
         }
-        size = m_MeshRenderer.bounds.size;
+        
+        Vector3 localSize = m_Mesh.bounds.size;
+        Vector3 scale = transform.lossyScale; // 世界缩放
+        size = Vector3.Scale(localSize, scale); // 按分量相乘
     }
 
     public void GetDirection(out Vector3 up, out Vector3 right, out Vector3 forward)
